7d6c8a9fbddf68715ed14ece037d0158ea0f88f8
Chirag Chovatiya <chirag.c@webcodegenie.com>
2021-09-06 20:05:37 +0530

    Initial Commit
    Doc file added

---
+++ 06-09-2021/nodedemo.js
@@ -0,0 +1,42 @@
+console.log('Welcome here...');
+
+// area of triangle
+var l = 25;
+var w = 10;
+console.log('Area of triangle is ' + (l * w));
+
+// area of square
+var s = 20;
+console.log('Area of square is ' + (s * s));
+
+// area of circle
+var r = 10;
+var pi = 3.14;
+console.log('Area of circle is ' + (pi * r * r));
+
+//Average of 5 numbers.
+var a, b, c, d, e;
+a = 6;
+b = 7;
+c = 8;
+d = 9;
+e = 10;
+
+var res = (a + b + c + d + e) / 5;
+console.log('Average of five numbers are ' + res);
+
+//check smallest/largest number
+if (a < b) {
+    console.log("A is lesser than B");
+}
+else {
+    console.log("A is greater than B");
+}
+
+//check odd-even number
+if (a % 2 == 0) {
+    console.log("A is EVEN number");
+}
+else {
+    console.log('A is ODD number');
+}
