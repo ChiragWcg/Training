a58486df8b5274c4fa6b51c0c760591376028ec1
Chirag Chovatiya <chirag.c@webcodegenie.com>
2021-09-07 11:18:10 +0530

    Initial Commit

--- 06-09-2021/nodedemo.js
+++ 06-09-2021/nodedemo.js
@@ -40,3 +40,65 @@ if (a % 2 == 0) {
 else {
     console.log('A is ODD number');
 }
+
+//check leap year or not
+var year = 2022;
+if (year % 4 == 0 || year % 400 == 0) {
+    if (year % 100 != 0) {
+        console.log(year + " is a Leap year");
+    }
+    else {
+        console.log(year + ' is not leap year');
+    }
+}
+else {
+    console.log(year + ' is not leap year');
+}
+
+//check zero, positive or negative
+var n = -27;
+if (n == 0) {
+    console.log('You have entered ZERO');
+}
+else if (n > 0) {
+    console.log(n + ' is Positive Value');
+}
+else {
+    console.log(n + ' is Negative value');
+}
+
+var j = 5;
+var fact = 1;
+for (i = 1; i <= j; i++) {
+    fact = fact * i;
+}
+console.log('Factorial of ' + j + " is " + fact);
+
+
+//swap two numbers
+console.log('A = ' + a);
+console.log('B = ' + b);
+
+c = a;
+a = b;
+b = c;
+
+console.log('A = ' + a);
+console.log('B = ' + b);
+
+
+//print square of entered number if n<10
+var n = 6;
+if (n < 10) {
+    console.log('square of ' + n + ' is ' + n * n);
+}
+else {
+    console.log('You entered greatest number than 10');
+}
+
+//multiplication table
+console.log('Multiplication table');
+for (i = 1; i <= n; i++) {
+    console.log(n * i);
+}
+
